20413a6ca5bde8eac72e5fc1df6469068317b194
Lxy <603877003@qq.com>
2021-12-01 20:36:36 +0800

    test

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+/**
+ * @Auther lxy
+ * @Date 2021/12/1 - 12 -01 -20:28
+ * @Description HelloWo
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("HelloWorld");
+    }
+}
